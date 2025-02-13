0d6d632ed843f91f47d5897c476a1d41a6a34dca
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-02-13 10:09:24 +0100

    refactor: texts taken to ex file

--- cabAssist.py
+++ cabAssist.py
@@ -8,39 +8,12 @@
 import time
 import os
 import sys
-from .ride import Ride
-from .timer import Timer
-
+from ride import *
+from timer import *
+from texts import *
 
 Rides = []
-txt_logo_1 = "\t\t   _____      _                        _     _   "
-txt_logo_2 = "\t\t  / ____|    | |         /\           (_)   | |  "
-txt_logo_3 = "\t\t | |     __ _| |__      /  \   ___ ___ _ ___| |_ "
-txt_logo_4 = "\t\t | |    / _` | '_ \    / /\ \ / __/ __| / __| __|"
-txt_logo_5 = "\t\t | |___| (_| | |_) |  / ____ \\__ \__ \ \__ \ |_"
-txt_logo_6 = "\t\t  \_____\__,_|_.__/  /_/    \_\___/___/_|___/\__|"
-txt_logo_7 = "\t\t\tSolution provided by FERGARCAT@GITHUB\n"
-txt_welcome = "\t\t\t*** Welcome to Cab Assist! ***\nThis application is developed for educational purpose within the AI Bootcamp @FactoríaF5 \n""\nThis app offers taxi drivers a light and reliable solution to trace their rides.\nYou will be able to keep record of all your rides, change your fees and charge your customers.\n\n\t\t\t*** IMPLEMENTED FEATURES ***\n\t\t\tBASIC LEVEL:\n\t\t\t- Shows welcome message(this).\n\t\t\t- Start a new ride.\n\t\t\t- Keep record of the waiting fare.\n\t\t\t- Keep record of the riding fare.\n\t\t\t- End ride and calculate total fare.\n\t\t\t- Start a new ride without closing the app.\n\t\t\tMEDIUM LEVEL:\n\t\t\t- Setup Fee Options.\n\t\t\tADVANCED LEVEL:\n\t\t\t- Use of OOP code.\n\n\t\tVisit https://github.com/fergarcat/cabassist for more details."
-txt_options = "\n\t\t\tPlease select one OPTION.\n"
-txt_menu_home = "\t\t\t(R) To Start a New Ride\n\n\t\t\t(S) To SETUP Fees\n\n\t\t\t(Q) To Quit\n\n"
-txt_msg_wait = "\t\t\t(D) Press To DRIVE\n\n\t\t\t(E) To END ride\n\n"
-txt_msg_drive = "\t\t\tPress S to STOP driving.\n\t\t\tRide cant't be finished while driving."
-txt_quit = "***Thanks for using Cab Assist. We are looking to see you again soon!***"
-txt_wrong_input = "\t\t\t*** WRONG INPUT ***"
-txt_run_length = "\t\t\tYou\'ve been driving for: "
-txt_wait_length = "\t\t\tYou\'ve been waiting for: "
-txt_sec = "seconds"
-txt_ride_start = "\t\t\tYour ride has started at: "
-txt_meter_start = "\n\t\t\tYou are STOPPED. Meter is running.\n\n"
-txt_current_time = "\t\t\tCurrent time: "
-txt_end = "\t\t\tYour drive has FINISHED. Total FARE: "
-txt_press_enter = "\n\t\t\tPress any KEY to continue..."
-txt_last_drives = "\tLast 3 DRIVES: \n"
-txt_current_drive_fee = "\t\tCurrent Drive Fee: "
-txt_current_wait_fee = " Current Wait Fee: "
-txt_msg_setup = "\n\n\t\t\t*** Setup Fees ***\n\n\t\t\t(D) To change Drive Fee\n\t\t\t(W) To change Wait Fee\n\t\t\t(M) Back to MAIN Menu\n"
-txt_enter_drive_fee = "\n\n\t\t\tEnter new Drive Fee: "
-txt_enter_wait_fee = "\n\n\t\t\tEnter new Wait Fee: "
+
 waitfee = 0.02
 drivefee = 0.05
 
